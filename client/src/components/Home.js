672e8ab07e8f2161c60e3c4cc026549eb48fcc09
howon <howon408@gmail.com>
2020-09-17 12:56:19 +0900

    홈화면, 유저정보화면, 포스팅추가화면 제작

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,80 +4,98 @@ import Logo from '../image/logo2.PNG';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import SearchIcon from '../image/search-icon.png';
-
+import PostingAddBtn from '../image/posting-add-btn.png';
+import { Typography, IconButton} from '@material-ui/core';
 
 
 class Home extends Component{
+    
     render(){
      return (
-         <>
-         <div className="left-page">
-            <div className="header">
-                <div className="logo">
-                    <img src={Logo} height="70px"/>
+         <div className="full-page">
+            <div className="left-page">
+                <div className="header">
+                    <div className="logo">
+                        {/* <img src={Logo} height="70px"/> */}
+                    </div>
+                    <div className="login-btn">
+                        <Button type="submit" variant="contained" color="primary">로그인</Button>
+                    </div>
                 </div>
-                <div className="login-btn">
-                    <Button type="submit" variant="outlined">로그인</Button>
+                <div className="left-page-posting">
+                    <div className="posting-add-btn">
+                        <IconButton>
+                            <img height="40px" src={PostingAddBtn}/>
+                        </IconButton>
+                    </div>
+                    sns posting 화면
                 </div>
             </div>
-            <div className="left-page-posting">
-                sns posting 화면
+            <div className="right-page">
+                <div className="right-search-bar">
+                    <form className="search-bar">
+                        <input type="text" placeholder="Search..." aria-label="Search" />
+                        <IconButton>
+                        <img src={SearchIcon}/>
+                        </IconButton>
+                    </form>
+                </div>
+                <div className="tag-selection">
+                    <p>원하는 태그 선택</p>
+                    <div className="tag-selection-box">
+                        <from>
+                            <ul>
+                            <div className="line4"></div>
+                                <li><Button style={{fontSize:'18px',fontWeight:'bold'}}>상의</Button></li>
+                                <li><Button>반팔</Button></li>
+                                <li><Button>셔츠</Button></li>
+                                <li><Button>후드티</Button></li>
+                                <li><Button>스웨트</Button></li>
+                                <li><Button>기타</Button></li>
+                            </ul>
+                            <div className="line1"></div>
+                            <ul>
+                                <li><Button style={{fontSize:'18px',fontWeight:'bold'}}>아우터</Button></li>
+                                <li><Button>코트</Button></li>
+                                <li><Button>후드집업</Button></li>
+                                <li><Button>가디건</Button></li>
+                                <li><Button>자켓</Button></li>
+                                <li><Button>패딩</Button></li>
+                            </ul>
+                            <div className="line2"></div>
+                            <ul>
+                                <li><Button style={{fontSize:'18px',fontWeight:'bold'}}>하의</Button></li>
+                                <li><Button>데님</Button></li>
+                                <li><Button>스커트</Button></li>
+                                <li><Button>슬랙스</Button></li>
+                                <li><Button>레깅스</Button></li>
+                                <li><Button>기타</Button></li>
+                            </ul>
+                            <div className="line3"></div>
+                            <ul>
+                                <li><Button style={{fontSize:'18px',fontWeight:'bold'}}>신발</Button></li>
+                                <li><Button>운동화</Button></li>
+                                <li><Button>구두</Button></li>
+                                <li><Button>스니커즈</Button></li>
+                                <li><Button>샌들</Button></li>
+                                <li><Button>기타</Button></li>
+                            </ul>
+                        </from>
+                    </div>
+                </div>
+                <div className="hot-posting">
+                    <p>인기 포스터</p>
+                    <div className="hot-posting-box">
+                    </div>
+                </div>
             </div>
-         </div>
-         <div className="right-page">
-             <div className="right-search-bar">
-                <form className="search-bar">
-                    <input type="text" placeholder="Search..."
-                        aria-label="Search" />
-                    <img src={SearchIcon}/>
-                </form>
-             </div>
-             <div className="tag-selection">
-                <p>원하는 태그 선택</p>
-                <div className="tag-selection-box">
-                    <from>
-                        <ul>
-                            <li><Button >상의</Button></li>
-                            <li><Button>반팔</Button></li>
-                            <li><Button>셔츠</Button></li>
-                            <li><Button>후드티</Button></li>
-                            <li><Button>스웨트</Button></li>
-                        </ul>
-                        <ul>
-                            <li><Button>아우터</Button></li>
-                            <li><Button>코트</Button></li>
-                            <li><Button>후드집업</Button></li>
-                            <li><Button>가디건</Button></li>
-                            <li><Button>자켓</Button></li>
-                            <li><Button>패딩</Button></li>
-                        </ul>
-                        <ul>
-                            <li><Button>하의</Button></li>
-                            <li><Button>데님</Button></li>
-                            <li><Button>스커트</Button></li>
-                            <li><Button>슬랙스</Button></li>
-                            <li><Button>레깅스</Button></li>
-                        </ul>
-                        <ul>
-                            <li><Button>신발</Button></li>
-                            <li><Button>운동화</Button></li>
-                            <li><Button>구두</Button></li>
-                            <li><Button>스니커즈</Button></li>
-                            <li><Button>샌들</Button></li>
-                        </ul>
-                    </from>
-                </div>
-             </div>
-             <div className="hot-posting">
-                 <p>인기 포스터</p>
-                <div className="hot-posting-box">
-                </div>
-             </div>
-         </div>
-         <footer className="footer">
-            <img src={Logo} height="30px"/>
-         </footer>
-        </>
+            <footer className="footer">
+                <Typography variant="body2" color="textSecondary" align="center">
+                {'긴머리와 파마머리 '+new Date().getFullYear()+' 졸업작품 프로젝트'}
+                </Typography>
+            </footer>
+            
+        </div>
      );
      };
  }
--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import './CSS/UserInfo.css';
 import Setting from '../image/setting.PNG';
-import Heart from '../image/heart.PNG';
+import HomeIcon from '../image/home-icon.png';
+import UserPhoto from '../image/user-photo.png';
 import { Button } from '@material-ui/core';
 import {IconButton} from '@material-ui/core';
-
+import { Typography} from '@material-ui/core';
 
 
 class UserInfo extends Component{
     render(){
         return (
             <div className="all">
-            
                 <div className="header">
                     <div className="logo"></div>
-                    
-                    <div className="home-icon"></div>
-                    
-                    <div className="user-profile"></div>
+                    <div className="home-icon">
+                        <IconButton>
+                            <img width="40px" src={HomeIcon}/>
+                        </IconButton>
+                    </div>
+                    <div className="user-profile">
+                        <IconButton>
+                            <img width="45px" src={UserPhoto}/>
+                        </IconButton>
+                    </div>
                 </div>
                 <div className="user-area">
                     <div className="user-info">
@@ -28,14 +34,7 @@ class UserInfo extends Component{
                             <img width="30px" src={Setting}/>
                             </IconButton>
                         </div>
-                        <p>username</p>
-                        {/* <img className="heart" src={Heart}/> */}
-                        <div className="heart">
-                        <IconButton color="secondary">
-                        <img width="30px" src={Heart}/>
-                        </IconButton>
-                        </div>
-                        <p className="heart-num">100</p>
+                        <p className="user-name">username</p>
                         <div className="user-history">
                             <p className="history1">게시글</p>
                             <p className="history2">팔로워</p> 
@@ -52,6 +51,11 @@ class UserInfo extends Component{
                         유저 포스팅
                     </div>
                 </div>
+                <footer className="footer">
+                <Typography variant="body2" color="textSecondary" align="center">
+                {'긴머리와 파마머리 '+new Date().getFullYear()+' 졸업작품 프로젝트'}
+                </Typography>
+                </footer>
             </div>
         )
     }
